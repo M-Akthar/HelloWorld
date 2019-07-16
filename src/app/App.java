1a97d389d901fff4adb066aedd1e5c3ae762c9c2
M-Akthar <Muhammad.Aktharuzzaman@academytrainee.com>
2019-07-16 11:17:54 +0100

    Added functionality to add and retrieve strings from a java map

--- src/app/App.java
+++ src/app/App.java
@@ -7,6 +7,21 @@ public class App {
 
 		System.out.println("Hello world to the standard out");
 		
+		JMap jmap = new JMap();
+		
+		String userInput1 = "Object 1";
+		String userInput2 = "Object 2";
+		String userInput3 = "Object 3";
+		
+		jmap.addString(1, userInput1);
+		jmap.addString(2, userInput2);
+		jmap.addString(3, userInput3);
+		
+		System.out.println("------ Line Break ------");
+		
+		System.out.println(jmap.getStringFromMap(2));
+		System.out.println(jmap.getStringFromMap(1));
+		System.out.println(jmap.getStringFromMap(3));
 	}
 
 }
